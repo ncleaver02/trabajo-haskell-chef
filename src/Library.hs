aa6e79f5059299bb701335ea90c8a01b839e506c
ncleaver02 <ncleaver@frba.utn.edu.ar>
2026-06-01 02:11:54 -0300

    Hechas las partes A, B y C del trabajo

--- src/Library.hs
+++ src/Library.hs
@@ -3,3 +3,125 @@ import PdePreludat
 
 doble :: Number -> Number
 doble numero = numero + numero
+
+-- Parte A--------------------------------------------------------------------------------------------------------------------------------
+data Participante = Participante {
+    nombre :: String,
+    trucosDeCocina :: [Truco],
+    especialidad :: Plato
+} deriving (Show)
+
+data Plato = Plato {
+    dificultad :: Number,
+    componentes :: [Componente]
+} deriving (Show)
+
+data Componente = Componente {
+    ingrediente :: String,
+    peso :: Number
+} deriving (Show)
+
+type Truco = Plato -> Plato
+
+endulzar :: Number -> Truco
+endulzar unPeso unPlato = agregarComponente (Componente "Azucar" unPeso) unPlato
+
+agregarComponente :: Componente -> Truco
+agregarComponente unComponente (Plato dificultad componentes) =  Plato dificultad $ unComponente : componentes
+
+
+salar :: Number -> Truco
+salar unPeso unPlato = agregarComponente (Componente "Sal" unPeso) unPlato
+
+
+darSabor :: Number -> Number -> Truco
+darSabor pesoAzucar pesoSal unPlato = endulzar pesoAzucar . salar pesoSal $ unPlato
+
+
+duplicarPorcion :: Truco
+duplicarPorcion (Plato dificultad componentes) = Plato dificultad $ map (multiplicarPorcion 2) componentes
+
+multiplicarPorcion :: Number -> Componente -> Componente
+multiplicarPorcion unNumero (Componente ingrediente peso) = Componente ingrediente $ peso * unNumero
+
+
+simplificar :: Truco
+simplificar unPlato 
+   | cantidadComponentes unPlato > 5 && dificultad unPlato > 7 = cambiarDificultad 5 . quitarComponentesLigeros $ unPlato
+   | otherwise = unPlato
+
+cantidadComponentes :: Plato -> Number
+cantidadComponentes (Plato _ componentes) = length componentes
+
+cambiarDificultad :: Number -> Truco
+cambiarDificultad unNumero (Plato _ componentes) = Plato unNumero componentes
+
+quitarComponentesLigeros :: Truco
+quitarComponentesLigeros (Plato dificultad componentes) = Plato dificultad $ filter (not . masLigeroQue 10) componentes
+
+masLigeroQue :: Number -> Componente -> Bool
+masLigeroQue unNumero (Componente _ peso) = peso < unNumero
+
+
+esVegano :: Plato -> Bool
+esVegano unPlato = not (tiene "Carne" unPlato) && not (tiene "Huevo" unPlato) && not (tiene "Lacteo" unPlato)
+
+tiene :: String -> Plato -> Bool
+tiene unIngrediente (Plato _ componentes) = any (esIngrediente unIngrediente) componentes
+
+esIngrediente :: String -> Componente -> Bool
+esIngrediente unIngredienteBuscado (Componente ingrediente _) = unIngredienteBuscado == ingrediente
+
+esSinTacc :: Plato -> Bool
+esSinTacc unPlato = not (tiene "Harina" unPlato)
+
+
+esComplejo :: Plato -> Bool
+esComplejo unPlato = cantidadComponentes unPlato > 5 && dificultad unPlato > 7
+
+
+noAptoHipertension :: Plato -> Bool
+noAptoHipertension (Plato _ componentes) = any (masPesadoQue 2) . filter (esIngrediente "Sal") $ componentes
+
+masPesadoQue :: Number -> Componente -> Bool
+masPesadoQue unNumero (Componente _ peso) = peso > unNumero
+
+-- Parte B--------------------------------------------------------------------------------------------------------------------------------
+
+pepeRonccino :: Participante
+pepeRonccino = Participante {
+    nombre = "Pepe Ronccino",
+    trucosDeCocina = [darSabor 5 2, simplificar, duplicarPorcion],
+    especialidad = platoPepe
+}
+
+
+platoPepe :: Plato
+platoPepe = Plato {
+    dificultad = 8,
+    componentes = [Componente "Sal" 4, Componente "Azucar" 2, Componente "Carne" 5, Componente "Harina" 9, Componente "Lacteo" 4, Componente "Huevo" 1]
+}
+
+-- Parte C--------------------------------------------------------------------------------------------------------------------------------
+
+cocinar :: Participante -> Plato
+cocinar (Participante _ trucosDeCocina especialidad) = foldl aplicarTruco (especialidad) (trucosDeCocina)
+
+aplicarTruco :: Plato -> Truco -> Plato
+aplicarTruco unPlato unTruco = unTruco unPlato
+
+
+esMejorQue :: Plato -> Plato -> Bool
+esMejorQue unPlato otroPlato = dificultad unPlato > dificultad otroPlato && (sumaPesos . componentes) unPlato < (sumaPesos . componentes) otroPlato
+
+sumaPesos :: [Componente] -> Number
+sumaPesos unosComponentes = sum $ map peso unosComponentes
+
+
+participanteEstrella :: [Participante] -> Participante
+participanteEstrella listaParticipantes = foldl1 mejorParticipante listaParticipantes
+
+mejorParticipante :: Participante -> Participante -> Participante
+mejorParticipante unParticipante otroParticipante
+    | esMejorQue (cocinar unParticipante) (cocinar otroParticipante) = unParticipante
+    | otherwise = otroParticipante
--- src/Spec.hs
+++ src/Spec.hs
@@ -3,9 +3,77 @@ import PdePreludat
 import Library
 import Test.Hspec
 
+platoFideos :: Plato
+platoFideos = Plato 5 [Componente "Fideos" 200]
+
+platoEnsalada :: Plato
+platoEnsalada = Plato 4 [Componente "Lechuga" 50, Componente "Tomate" 50]
+
+platoPesado :: Plato
+platoPesado = Plato 8 [Componente "Sal" 3, Componente "Carne" 100, Componente "Cebolla" 5]
+
+platoRival :: Plato
+platoRival = Plato 4 [Componente "Roca" 500]
+
+rivalPepe :: Participante
+rivalPepe = Participante "Rival" [] platoRival
+
 correrTests :: IO ()
 correrTests = hspec $ do
   describe "Test de ejemplo" $ do
     it "El pdepreludat se instaló correctamente" $ do
       doble 1 `shouldBe` 2
 
+  describe "Parte A - Trucos de Cocina" $ do
+    it "endulzar agrega el componente 'Azucar' con el peso indicado al principio de la lista" $ do
+      (ingrediente . head . componentes) (endulzar 10 platoFideos) `shouldBe` "Azucar"
+
+    it "duplicarPorcion multiplica el peso de todos los componentes por 2" $ do
+      (peso . head . componentes) (duplicarPorcion platoFideos) `shouldBe` 400
+
+    it "simplificar le baja la dificultad a 5 a un plato complejo" $ do
+      dificultad (simplificar platoPepe) `shouldBe` 5
+      
+    it "simplificar quita los componentes de menos de 10g en un plato complejo" $ do
+      cantidadComponentes (simplificar platoPepe) `shouldBe` 0
+
+    it "simplificar no hace nada en un plato que NO es complejo" $ do
+      dificultad (simplificar platoFideos) `shouldBe` 5
+
+  describe "Parte A - Consultas sobre Platos" $ do
+    it "esVegano da True si no tiene carne, huevo ni lácteo" $ do
+      esVegano platoEnsalada `shouldBe` True
+
+    it "esVegano da False si tiene carne" $ do
+      esVegano platoPesado `shouldBe` False
+
+    it "esSinTacc da False si tiene Harina" $ do
+      esSinTacc platoPepe `shouldBe` False
+
+    it "esComplejo da True si tiene más de 5 componentes y dificultad mayor a 7" $ do
+      esComplejo platoPepe `shouldBe` True
+
+    it "noAptoHipertension da True si tiene un componente llamado Sal con más de 2 gramos" $ do
+      noAptoHipertension platoPepe `shouldBe` True
+      noAptoHipertension platoPesado `shouldBe` True
+
+  describe "Parte B - Modelo de Datos" $ do
+    it "La especialidad de Pepe Ronccino es un plato complejo" $ do
+      esComplejo (especialidad pepeRonccino) `shouldBe` True
+
+  describe "Parte C - Torneo y Evaluaciones" $ do
+    it "esMejorQue da True si el plato tiene estrictamente más dificultad y menos peso total" $ do
+      let platoPro = Plato 10 [Componente "Magia" 50]
+      let platoNoob = Plato 5 [Componente "Roca" 100]
+      esMejorQue platoPro platoNoob `shouldBe` True
+
+    it "esMejorQue da False si el plato tiene más dificultad pero también es más pesado" $ do
+      let platoPesadote = Plato 10 [Componente "Piedra" 1000]
+      let platoLiviano = Plato 5 [Componente "Pluma" 10]
+      esMejorQue platoPesadote platoLiviano `shouldBe` False
+
+    it "cocinar aplica correctamente los trucos y devuelve un plato diferente al original" $ do
+      dificultad (cocinar pepeRonccino) `shouldBe` 5
+
+    it "participanteEstrella devuelve al ganador de una lista de participantes" $ do
+      nombre (participanteEstrella [pepeRonccino, rivalPepe]) `shouldBe` "Pepe Ronccino"
